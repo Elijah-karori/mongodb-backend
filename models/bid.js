c00dcdff9cf2584cd7719e5d11aac760a5dd5279
Elijah-karori <elijakarori23@gmail.com>
2023-04-03 02:04:35 +0300

    new comments

--- models/bid.js
+++ models/bid.js
@@ -1,12 +1,13 @@
-const mongoose =require('mongoose')
+const mongoose =require('mongoose')//library to communicate with database
+//create a data schema to give parameters for CRUD operations, database enteries
 const auctionSchema = new mongoose.Schema({
-    name: String,
-    description: String,
-    startingBid: Number,
-    currentBid: Number,
-    image: String,
-    username: String,
+    name: String,//field data type
+    description: String,//field data type
+    startingBid: Number,//field data type
+    currentBid: Number,//field data type
+    image: String,//field data type
+    username: String,//field data type
   });
   
-  const Auction = mongoose.model('Auction', auctionSchema);
-module.exports= Auction
+  const Auction = mongoose.model('Auction', auctionSchema);// provide name to schema 
+module.exports= Auction//export as module
--- routes/bids.js
+++ routes/bids.js
@@ -18,41 +18,49 @@ router.post('/bid', async (req, res) => {
    console.log(auction)
    // update to database
     await Auction.updateOne({name:auction.name}, { currentBid: req.body.bidAmount, username:req.body.username});
-   
+   //send a response for success with 200 success code
     res.status(200).send('Bid successful');
-  } catch (err) {
+  } catch (err) {// catch error and log error without stoping the server
     console.error(err);
-    res.status(500).send('Internal server error');
+    res.status(500).send('Internal server error');//send a response for error with 500 server error code
   }
-});
+});// route for payment
 
 router.post('/charge', async (req, res) => {
   try {
+    //check the auction if availble
     const auction = await Auction.findOne({name:req.body.name});
+    // provide  message for unavaiable auction
     console.log(auction)
     if (!auction) {
       return res.status(404).send('Auction not found');
     }
+    //provide username in a variable
     const winnerName=req.body.username
+    //check if the winner username is the same as the user username
     if(auction.username != winnerName || (null || undefined )){
-      return res.status(404).send('username not found');
+      return res.status(404).send('username not found');// response for username not the same as winner
     }
+    // provide user payment amount
      const amount = auction.currentBid; 
+     //rovide payment options response
     res.status(200).send('Payment with MPESA send money (07 XXXXXXXX)or pochi la biashara. Amount: '+ amount);
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal server error');
   }
 });
-
+// route to get auctions available
 router.get('/auctions', async (req, res) => {
   try {
+    //get auctions from a database 
     const auctions = await Auction.find();
+    //response with all auctions
     res.status(200).json(auctions);
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal server error');
   }
 });
-
+//export the function as internal module
 module.exports = router
